a906b0bf376ffa0f44c4cbc2f85e869d8300ac5b
Gan Zejun <ganzejun@zhongan.io>
2019-09-18 19:38:48 +0800

    sample code

--- strategy.py
+++ strategy.py
@@ -8,17 +8,13 @@ from og_solver import OGSolver
 
 KAFKA_TOPIC = 'hack-final-test'
 KAFKA_SERVER = ['47.100.222.11:30000']
-API_URL = "http://172.28.152.102:38000"
+API_URL = "http://172.28.152.102:30020"
 
 private_key = "af1b6df8cc06d79902029c0e446c3dc2788893185759d2308b5bb10aa0614b7d"
 TOKEN = '98765467890'
 
 og = OGSolver(API_URL, TOKEN)
 my_account = Account(private_key)
-
-KAFKA_TOPIC = 'hack-final-test'
-KAFKA_SERVER = ['47.100.222.11:30000']
-API_URL = "http://172.28.152.102:38000"
 
 
 def on_new_tx(tx):
@@ -34,11 +30,13 @@ def on_new_tx(tx):
         # j = og.query_sequencer_by_height(tx['data']['height'])
         # # {'type': 1, 'data': {'type': 1, 'hash': '0x2c4f87904d2ba37512511506a102b638bfcff8534b652dd288c498372b4bb6ee', 'parents': ['0xda2ea8f640300ffe8ae27ee7ce63ca012e24fb06ce170089dfaeb952cdb21c70'], 'from': '0x7349f7a6f622378d5fb0e2c16b9d4a3e5237c187', 'nonce': 65754, 'treasure': '1000', 'height': 65754, 'weight': 65757}}
         # print('seq', j)
+        resp = og.query_nonce(my_account.address)
+        nonce = resp['data']
+        og.send_tx(my_account, [tx['data']['hash']], nonce + 1, my_account.address, 1, my_account.public_key)
 
 
 if __name__ == '__main__':
     # load accounts
-
 
     # this is the callback for receiving a new tx:
     consumer = KafkaConsumer(KAFKA_TOPIC, bootstrap_servers=KAFKA_SERVER)
